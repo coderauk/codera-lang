0af5cbdbf0ad7f53a65d141aea631b51873285f7
Andy Stewart <andy.stewart@codera.co.uk>
2016-04-13 15:58:21 +0100

    Ported over the ClasspathResource from another of our projects

---
+++ src/main/java/uk/co/codera/lang/io/ClasspathResource.java
@@ -0,0 +1,27 @@
+package uk.co.codera.lang.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+
+public class ClasspathResource {
+
+	private final String path;
+	
+	public ClasspathResource(String path) {
+		this.path = path;
+	}
+	
+	public String getAsString() {
+		try {
+			return IOUtils.toString(getAsStream());
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	private InputStream getAsStream() {
+		return ClasspathResource.class.getResourceAsStream(this.path);
+	}
+}
---
+++ src/test/java/uk/co/codera/lang/io/ClasspathResourceTest.java
@@ -0,0 +1,14 @@
+package uk.co.codera.lang.io;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class ClasspathResourceTest {
+
+	@Test
+	public void shouldBeAbleToReadContentsOfFileThatExists() {
+		assertThat(new ClasspathResource("/readme.txt").getAsString(), is("This is a readme file"));
+	}
+}
