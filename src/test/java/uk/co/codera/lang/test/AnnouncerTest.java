62af5bc1c7fedc863866a6641a3c18c777cb37b6
Andy Stewart <andy.stewart@codera.co.uk>
2016-04-25 08:50:27 +0100

    Tests around TimeMeasure

--- src/test/java/uk/co/codera/lang/test/AnnouncerTest.java
+++ src/test/java/uk/co/codera/lang/test/AnnouncerTest.java
@@ -11,7 +11,6 @@ import static org.mockito.Mockito.verify;
 
 import java.util.Arrays;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- src/test/java/uk/co/codera/lang/test/datetime/TimeMeasureTest.java
+++ src/test/java/uk/co/codera/lang/test/datetime/TimeMeasureTest.java
@@ -2,6 +2,7 @@ package uk.co.codera.lang.test.datetime;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static uk.co.codera.lang.datetime.TimeMeasure.milliseconds;
 import static uk.co.codera.lang.datetime.TimeMeasure.seconds;
 
 import java.util.concurrent.TimeUnit;
@@ -13,9 +14,21 @@ import uk.co.codera.lang.datetime.TimeMeasure;
 public class TimeMeasureTest {
 
     @Test
-    public void testConvertToNewUnit() {
+    public void shouldConvertFromSecondsToMilliseconds() {
         TimeMeasure newTimeMeasure = seconds(10).convert(TimeUnit.MILLISECONDS);
         assertThat(newTimeMeasure.quantity(), is(10000L));
         assertThat(newTimeMeasure.unit(), is(TimeUnit.MILLISECONDS));
+    }
+
+    @Test
+    public void shouldConvertFromMillisecondsToSeconds() {
+        TimeMeasure newTimeMeasure = milliseconds(1000L).convert(TimeUnit.SECONDS);
+        assertThat(newTimeMeasure.quantity(), is(1L));
+        assertThat(newTimeMeasure.unit(), is(TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void shouldLosePrevisionWhenConvertingFromMillisecondsToSeconds() {
+        assertThat(milliseconds(1230L).convert(TimeUnit.SECONDS).quantity(), is(1L));
     }
 }
