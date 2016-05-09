261d0153a87b435b902e2dabf81bc00d36520fc9
Andy Stewart <andy.stewart@codera.co.uk>
2016-05-09 14:40:42 +0100

    Added hashCode and equals methods for Fraction

--- src/main/java/uk/co/codera/lang/math/Fraction.java
+++ src/main/java/uk/co/codera/lang/math/Fraction.java
@@ -1,5 +1,8 @@
 package uk.co.codera.lang.math;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class Fraction {
 
     private final int nominator;
@@ -17,6 +20,24 @@ public class Fraction {
 
     public static Fraction from(int numerator, int denominator) {
         return new Fraction(numerator, denominator);
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this.nominator).append(this.denominator).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Fraction)) {
+            return false;
+        }
+        Fraction other = (Fraction) obj;
+        return new EqualsBuilder().append(this.nominator, other.nominator).append(this.denominator, other.denominator)
+                .isEquals();
     }
 
     @Override
--- src/test/java/uk/co/codera/lang/math/FractionTest.java
+++ src/test/java/uk/co/codera/lang/math/FractionTest.java
@@ -1,6 +1,8 @@
 package uk.co.codera.lang.math;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
@@ -21,5 +23,35 @@ public class FractionTest {
     @Test
     public void shouldPrintFractionAsToString() {
         assertThat(Fraction.from("1/2").toString(), is("1/2"));
+    }
+
+    @Test
+    public void shouldNotBeEqualIfOtherObjectIsNull() {
+        assertThat(Fraction.from("1/2"), is(not(equalTo(null))));
+    }
+
+    @Test
+    public void shouldNotBeEqualIfOtherObjectIsNotAFraction() {
+        assertThat(Fraction.from("1/2"), is(not(equalTo(new Object()))));
+    }
+
+    @Test
+    public void shouldNotBeEqualIfFractionDifferent() {
+        assertThat(Fraction.from("1/2"), is(not(equalTo(Fraction.from("1/3")))));
+    }
+
+    @Test
+    public void shouldBeEqualIfFractionTheSame() {
+        assertThat(Fraction.from("1/2"), is(equalTo(Fraction.from("1/2"))));
+    }
+
+    @Test
+    public void shouldHaveSameHashCodeIfFractionsAreEqual() {
+        assertThat(Fraction.from("1/2").hashCode(), is(equalTo(Fraction.from("1/2").hashCode())));
+    }
+
+    @Test
+    public void shouldHaveDifferentHashCodeIfFractionsAreNotEqual() {
+        assertThat(Fraction.from("1/2").hashCode(), is(not(equalTo(Fraction.from("1/3").hashCode()))));
     }
 }
